9e05c4a4fc3628e9df9a00c0e152031d7a3b1265
SnailTowarThesun <hanvskun@hotmail.com>
2016-01-20 22:57:34 +0800

    backup

--- web/web/urls.py
+++ web/web/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 import blog
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/',include('blog.urls')),
+    url(r'^',include('blog.urls')),
 ]
