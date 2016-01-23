2ab83caa9a25c9869e41a35dfdb439983a78a49a
SnailTowarThesun <hanvskun@hotmail.com>
2016-01-23 22:06:30 +0800

    add files

--- web/blog/urls.py
+++ web/blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
         url(r'^about',views.aboutMe,name='abhout'),
         url(r'^contact',views.contact,name='contact'),
         url(r'^post',views.post,name='post'),
-        url(r'^article/(?P<article_file>\w+)/$',views.article,name='article_detail'),
+        url(r'^article/(?P<article_file>\w+)/$',views.article_page,name='article_detail'),
+		url(r'^page(?P<page_list_num>\w+)/$',views.page_list,name='get_page_list'),
         ]
--- web/blog/views.py
+++ web/blog/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
-
+from models import *
 # Create your views here.
 def homePage(request):
-    # get blog lists here 
-    return render(request,'index.html')
+    # get top five blogs here
+    latest_article = article.objects.order_by('-article_published_time')[:5]
+    context = {'latest_article':latest_article}
+    return render(request,'index.html',context)
 
-def article(request,article_file):
+def page_list(request,page_list_num):
+    # get the blog begin with blog_begin_num
+    list_article = article.objects.order_by('-article_published_time')[int(page_list_num) - 1:int(page_list_num) + 4]
+    # get the number of all pages.
+    # PS: the max size of blogs in one page is 5
+    context = {'list_article':list_article}
+    return render(request,'pagelist.html',context)
+
+def article_page(request,article_file):
     # get comments here 
     return render(request,'BlogPages/' + article_file + '.html')
 
