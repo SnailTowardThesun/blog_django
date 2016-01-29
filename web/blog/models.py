76a9f9efce7c77065a17bde634674d8739a69486
SnailTowarThesun <hanvskun@hotmail.com>
2016-01-29 22:16:12 +0800

    change the model

--- web/blog/models.py
+++ web/blog/models.py
@@ -16,9 +16,11 @@ class article(models.Model):
     article_context_field = models.CharField(max_length = 100)
 
 class comment(models.Model):
-    def __str__(self):
-        return self.article_UUID_string
     article_UUID_string = models.CharField(max_length = 200) #unique key.format is title + date ,e.g: first_blog_2016_01_22
     comment_author = models.CharField(max_length=200)
     comment_published_time = models.DateTimeField()
     comment_context = models.CharField(max_length = 5000)
+    comment_author_phone_number = models.CharField(max_length=20)
+    comment_author_email_address = models.CharField(max_length = 200)
+    def __str__(self):
+        return self.article_UUID_string
