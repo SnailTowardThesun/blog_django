ebb302b32e97919d94dbf3da42a90bd8376e80ce
ME_Kun_Han <hanvskun@hotmail.com>
2016-02-02 21:33:02 +0800

    finish the pagination

--- web/blog/views.py
+++ web/blog/views.py
@@ -16,7 +16,18 @@ def page_list(request, page_list_num):
     list_article = Article.objects.order_by('-article_published_time')[(i_page_num - 1) * 2:(i_page_num - 1) * 2 + 2]
     # get the number of all pages.
     # PS: the max size of blogs in one page is 5
-    list_page = range(1, int(Article.objects.count() / 2) + 2)
+    if i_page_num < 2:
+        previous = 1
+    else:
+        previous = i_page_num - 1
+
+    count = Article.objects.all().count()
+    print("the i_page_num is ", i_page_num," the article's count is ", count)
+    if i_page_num * 2 > count:
+        next_page = i_page_num
+    else:
+        next_page = i_page_num + 1
+    list_page = [previous, next_page, count]
     context = {
         'list_article': list_article,
         'list_page': list_page,
